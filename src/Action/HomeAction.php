036ad5b8759ec9078b7e4d41b660089316d81f69
emilien letestu <emilien@pc32.home>
2018-06-17 18:36:44 +0200

    minor changes to homeAction

--- src/Action/HomeAction.php
+++ src/Action/HomeAction.php
@@ -8,10 +8,7 @@
 
 namespace App\Action;
 
-
-use App\Entity\Tech;
 use App\Responder\HomeResponder;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
