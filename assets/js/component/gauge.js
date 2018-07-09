858501d67773b1f4418664d6de72b5849844ae07
emilien letestu <emilien@pc32.home>
2018-07-09 19:54:23 +0200

    [js] code indent

--- assets/js/component/gauge.js
+++ assets/js/component/gauge.js
@@ -1,9 +1,9 @@
 
-const font = '35px Arial';
+const font               = '35px Arial';
 const supportCircleColor = "#eaeaea";
-const highLevelColor = "#b9f6ca";
-const mediumLevelColor = "#fff176";
-const lowLevelColor =   "#ff8a80";
+const highLevelColor     = "#b9f6ca";
+const mediumLevelColor   = "#fff176";
+const lowLevelColor      = "#ff8a80";
 
 class Gauge {
 
@@ -32,9 +32,10 @@ class Gauge {
      percentToDegrees(canvasId) {
 
         let idNumber = canvasId.split('_');
-        let elem = document.getElementById('skill_' + idNumber[1]);
-        let value = elem.value;
-        let percent = parseInt(value) / 100;
+        let elem     = document.getElementById('skill_' + idNumber[1]);
+        let value    = elem.value;
+        let percent  = parseInt(value) / 100;
+
         return 360 * percent;
     }
 
@@ -42,14 +43,14 @@ class Gauge {
 
         for (let i = 0; i < this.canvasList.length; i++) {
             let canvasId = this.canvasList[i].getAttribute('id');
-            let degrees = this.percentToDegrees(canvasId);
-            let canvas = document.getElementById(canvasId);
-            let context = canvas.getContext('2d');
-            let width = canvas.width;
-            let height = canvas.height;
-            let radians = degrees * Math.PI / 180;
-            let text = (degrees / 360) * 100 + '%';
-            let color = this.pickColor((degrees / 360) * 100);
+            let degrees  = this.percentToDegrees(canvasId);
+            let canvas   = document.getElementById(canvasId);
+            let context  = canvas.getContext('2d');
+            let width    = canvas.width;
+            let height   = canvas.height;
+            let radians  = degrees * Math.PI / 180;
+            let text     = (degrees / 360) * 100 + '%';
+            let color    = this.pickColor((degrees / 360) * 100);
 
             //draw circle
             context.beginPath();
@@ -63,6 +64,7 @@ class Gauge {
             context.strokeStyle = color;
             //deduct 90° from from the angles to start drawing from top end
             context.arc(width / 2, height / 2, 80, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
+
             context.shadowColor = '#444444';
             context.shadowBlur = 5;
             context.lineWidth = 30;
