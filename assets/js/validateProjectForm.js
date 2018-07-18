cb405f6390069626b0ee13e3d147a4ddb7d6bdd3
emilien letestu <emilien@pc32.home>
2018-07-18 11:12:48 +0200

    [js] project form validation

---
+++ assets/js/validateProjectForm.js
@@ -0,0 +1,13 @@
+import {ValidateForm} from "./class/validateForm";
+
+document.getElementById('edit_project_name').addEventListener('keyup', function () {
+    new ValidateForm('edit_project_name', 2, 100).validateTextLength();
+});
+
+document.getElementById('edit_project_pictRef').addEventListener('change', function () {
+    new ValidateForm('edit_project_pictRef', null, null, ["png", "gif", "jpg", "jpeg"]).validateFile();
+});
+
+document.getElementById('edit_project_description').addEventListener('keyup', function () {
+    new ValidateForm('edit_project_description', 10, 50).validateWordCount();
+});
