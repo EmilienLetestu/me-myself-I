ba59af35659fe6cb653575d4b3758b6eceed42c5
emilien letestu <emilien@pc32.home>
2018-07-18 11:10:59 +0200

    [js] add missing validation on skill form

--- assets/js/validateSkillForm.js
+++ assets/js/validateSkillForm.js
@@ -3,3 +3,7 @@ import {ValidateForm} from "./class/validateForm";
 document.getElementById('edit_skill_name').addEventListener('keyup', function () {
     new ValidateForm('edit_skill_name', 2, 100).validateTextLength();
 });
+
+document.getElementById('edit_skill_level').addEventListener('keyup', function () {
+    new ValidateForm('edit_skill_level', 10, 100).validateInteger();
+});
