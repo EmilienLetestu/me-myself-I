f405908e09a4134040097ec4d3f04c86f48f7e7a
emilien letestu <emilien@pc32.home>
2018-07-19 15:28:20 +0200

    [js/templates] handle delete and project publication update via Ajax

--- assets/js/class/ajax.js
+++ assets/js/class/ajax.js
@@ -1,3 +1,7 @@
+
+const red = '#ef5350';
+const green = '#00e676';
+
 class Ajax {
 
     constructor(
@@ -14,15 +18,46 @@ class Ajax {
 
         this.event.preventDefault();
         this.xmlhttp.onreadystatechange = function () {
-            if(this.readyState === 4){
-                this.responseText === 'success' ? alert('succes') : alert('error')
+
+            if(this.readyState === 4 ){
+
+                alert(this.responseText);
+
             }
         };
+
+        if(this.xmlhttp.responseText !== "Aucune correpondance trouvée"){
+
+            let checkId = this.id.split('-');
+            checkId[0] === 'update' ?
+                this.updatePublicationIcon() :
+                this.updateTable()
+            ;
+        }
 
         this.xmlhttp.open("GET",this.url,true);
         this.xmlhttp.send();
 
+
     }
+
+    updatePublicationIcon() {
+        let icon = document.getElementById(this.id).getElementsByTagName("i");
+        let iconType = icon[0].innerText;
+
+        iconType === 'close' ? icon[0].innerText = 'check' : icon[0].innerText = 'close';
+        iconType === 'check' ? icon[0].style.color = red : icon[0].style.color = green;
+    }
+
+    updateTable(){
+
+        let id = this.id.split('-');
+        let rowId = 'row-'+id[1]+'-'+id[2];
+
+        document.getElementById(rowId).remove();
+    }
+
+
 }
 
 export {Ajax}
--- assets/js/deleteProject.js
+++ assets/js/deleteProject.js
@@ -1,8 +1,6 @@
 import {Ajax} from "./class/ajax";
 
-let link = document.getElementsByClassName('delete-entity');
-
-console.log(link.length);
+let link = document.getElementsByClassName('crud-ajax');
 
 for(let i = 0; i < link.length; i++){
     link[i].addEventListener('click', function () {
